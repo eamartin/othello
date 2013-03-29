ec52c487255ef34e7bcf39c46090935087bca292
Eric Martin <e.a.martin1337@gmail.com>
2013-03-28 23:48:38 -0700

    Modified board a lot, haven't tested or compiled yet.

--- board.rs
+++ board.rs
@@ -47,13 +47,77 @@ fn get_bit(data: u64, pos: int) -> bool {
 
 #[inline]
 fn set_bit(data: &mut u64, pos: int) {
-    *data = *data | (1u64 << pos);
+    *data |= (1u64 << pos);
 }
 
 #[inline]
 fn get_index(pos: Position) -> int {
     let Position(x, y) = pos;
     x + 8 * y
+}
+
+fn get_row(board_side: u64, row_num: int) -> u8 {
+    let mut row = 0u8;
+    for int::range(0, 8) |col_num| {
+        let index = 8 * row_num + col_num;
+        row += ((board_side & (1u64 << index)) >> (index - col_num - 1));
+    }
+    return row;
+}
+
+fn get_col(board_side: u64, col_num: int) -> u8 {
+    let mut col = 0u8;
+    for int::range(0, 8) |row_num| {
+        let index = 8 * row_num + col_num;
+        col += (board_side & (1u64 << index)) >> (index - row_num - 1);
+    }
+    return col;
+}
+
+fn get_positive_diag(board_side: u64, start: Position) -> u8 {
+    let mut pos = start;
+    let mut diag = 0u8;
+    let mut offset = 0;
+    while pos.in_bounds() {
+        diag += (board_side & (1u64 << get_index(pos))) >> (index - offset - 1);
+    }
+    return diag;
+}
+
+
+fn get_linear_moves(my_row: u8, other_row: u8) -> ~[int] {
+    let get_bit = |data: u8, idx| (data & (1u8 << idx)) != 0;
+    let mut moves = vec::with_capacity<int>(8);
+
+    for int::range(1, 7) |index| {
+        if get_bit(other_row, index) {
+            // first lets look for move on the left
+            if !get_bit(my_row | other_row, index - 1) {
+                for int::range(index + 1, 8) |j| {
+                    if get_bit(my_row, j) {
+                        moves.push(index - 1);
+                        break;
+                    }
+                    if !get_bit(my_row | other_row) {
+                        break;
+                    }
+                }
+            }
+
+            // look for move on the right
+            if !get_bit(my_row | other_row, index + 1) {
+                for int::range_rev(index - 1, -1) |j| {
+                    if get_bit(my_row, j) {
+                        moves.push(index + 1);
+                        break;
+                    }
+                    if !get_bit(my_row | other_row) {
+                        break;
+                    }
+                }
+            }
+        }
+    }
 }
 
 #[deriving(Eq)]
@@ -100,21 +164,44 @@ pub impl Board {
     Get moves that are available to side.
     */
     fn get_moves(&self, side: Color) -> ~[Position] {
-        let other = side.other();
-        let mut moves: ~[Position] = ~[];
+        let mut moves = vec::with_capacity<Position>(32);
 
-        for int::range(0, 8) |y| {
-            for int::range(0, 8) |x| {
-                let pos = Position(x, y);
-                match self.get(pos) {
-                    Some(color) if color == other => {
-                        moves.push_all(self.get_moves_around_stone(side, pos))
-                    }
-                    _ => ()
-                };
+        let my_stones = self.get_stones(side);
+        let other_stones = self.get_stones(side.other());
+
+        // rows
+        for int::range(0, 8) |row_num| {
+            let my_row = get_row(my_stones, row_num);
+            let other_row = get_row(other_stones, row_num);
+
+            get_linear_moves(my_row, other_row).each |col_num| {
+                moves.push(Position(row_num, col_num));
             }
         }
-        return moves;
+
+        // column
+        for int::range(0, 8) |col_num| {
+            let my_col = get_col(my_stones, col_num);
+            let other_col = get_col(other_stones, col_num);
+
+            get_linear_moves(my_col, other_col).each |row_num| {
+                moves.push(Position(row_num, col_num));
+            }
+        }
+
+        // positive diagonals
+        let mut pos_starts = vec::from_fn(6, |x| Position(x, 0)) +
+            vec::from_fn(5, |y| Position(0, y + 1));
+        do pos_starts.each |start| {
+            let my_diag = get_positive_diag(my_stones, start);
+            let other_diag = get_positive_diag(other_stones, start);
+
+            get_linear_move(my_diag, other_diag).each |offset| {
+                moves.push(start.add(Position(offset, offset)));
+            }
+        }
+
+        // need to add negative diagonals
     }
 
     fn print_board(&self) {
@@ -127,44 +214,6 @@ pub impl Board {
                 };
             };
         };
-    }
-
-    /*
-    Given a spot (x, y) that contains a stone of side.other(), find moves available
-    to side.
-    */
-    fn get_moves_around_stone(&self, side: Color, pos: Position) -> ~[Position] {
-        let mut moves: ~[Position] = ~[];
-
-        for int::range(-1, 2) |dy| {
-            for int::range(-1, 2) |dx| {
-                let my_stones = self.get_stones(side);
-                let other_stones = self.get_stones(side.other());
-                let mut current_pos = pos.add(Position(dx, dy));
-                let this_move = current_pos;
-
-                // can't move there if not in bounds or if occupied
-                if (!current_pos.in_bounds() ||
-                    self.is_occupied(current_pos)) {
-                    loop
-                }
-
-                current_pos = pos.add(Position(-dx, -dy));
-                while (current_pos.in_bounds() &&
-                       get_bit(other_stones, get_index(current_pos))) {
-
-                    current_pos = current_pos.add(Position(-dx, -dy));
-                }
-
-                if (current_pos.in_bounds() &&
-                    get_bit(my_stones, get_index(current_pos))) {
-
-                    moves.push(this_move);
-                }
-            };
-        };
-
-        return moves;
     }
 
     fn make_move(&self, side: Color, pos: Position) -> Board {
